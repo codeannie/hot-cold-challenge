7ac61c463a47b98a52947675bdb2292b1c3e498d
Annie Chiang <devbear22@gmail.com>
2017-12-12 15:32:54 -0800

    initial styling

--- src/components/guessform.jsx
+++ src/components/guessform.jsx
@@ -12,15 +12,35 @@ export default class GuessForm extends React.PureComponent {
   // ref={node => this.inputNode = node} ?
   render() {
     return ( 
-      <form onSubmit={this.handleSubmit}>
-        <label for="SubmitAGuess">Enter a Number:</label>
-          <input type="text" name="guess-a-number" placeholder="Enter a Guess"/>   
-        <button type="submit" id="submit">Submit</button>
+      <form className="guessForm" onSubmit={this.handleSubmit} style={styles.guessForm}>
+        <label for="SubmitAGuess" className="guessLabel" style={styles.guessLabel}>Enter a Number:</label>
+          <input type="text" name="guessNumber" className="guessInput" style={styles.guessInput} placeholder="Enter a Guess"/>   
+        <button type="submit" id="submit" className="submitBtn" style={styles.submitBtn}>Submit</button>
       </form>
     )
   }
 }
 
-const style = {
-  
+const styles = {
+  guessForm: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  guessLabel: {
+    display: 'block',
+    textAlign: 'center'
+  },
+  guessInput: {
+    height: 'auto',
+    fontSize: 20,
+    border: 'none',
+    borderBottom: '2px solid red'
+  },
+  submitBtn: {
+    border: 0,
+    borderRadius: 3,
+    cursor: 'pointer',
+    padding: 7
+  }
 }

1ff117c1965ccf57c66bfd9ba87a00f4d47f0813
Annie Chiang <devbear22@gmail.com>
2017-12-13 18:50:10 -0800

    cleanup comments

--- src/components/generateNumber.js
+++ src/components/generateNumber.js
@@ -1,10 +1,4 @@
 
-// export default function getTargetNum() {
-//   let min = Math.ceil(min);
-//   let max = Math.floor(max);
-//   const targetNumber = Math.floor(Math.random() * (max - min + 1)) + min; 
-// }
-
 export default function targetNum() { 
   return Math.floor((Math.random() * 100) + 1);
 }
